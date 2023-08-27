e4bf5e963133605ea1796129788f9b241d1d89a2
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-08-27 15:38:09 -0400

    finished lesson

--- Multiple_Identical_Options_In_Switch_Statements.js
+++ Multiple_Identical_Options_In_Switch_Statements.js
@@ -7,5 +7,19 @@ function sequentialSizes(val) {
         case 3:
             answer = "Low";
             break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
     }
+    // Only change code above this line
+    return answer;
 }
+
+console.log(sequentialSizes(1))
