f08f3df3e97699c1dff764eb565b496b75829d58
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-08-27 15:30:42 -0400

    break and switch

---
+++ Multiple_Identical_Options_In_Switch_Statements.js
@@ -0,0 +1,11 @@
+function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+    }
+}
