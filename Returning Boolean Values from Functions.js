402526d4b3e8bd6bbb0c4200800064225bc23c86
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-01-22 19:38:29 -0500

    Finished lesson

--- Returning Boolean Values from Functions.js
+++ Returning Boolean Values from Functions.js
@@ -1,11 +1,5 @@
 function isLess(a, b) {
-    // Only change code below this line
-    if (a < b) {
-      return true;
-    } else {
-      return false;
-    }
-    // Only change code above this line
+    return a < b;
   }
   
   isLess(10, 15);
